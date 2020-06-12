6770c1ba939542d5e48a0cccfa84f023fc043112
Su <ssvagle@gmail.com>
2020-06-12 13:57:58 +0530

    added search class

---
+++ src/main/java/com/crm/qa/pages/SearchPage.java
@@ -0,0 +1,12 @@
+package com.crm.qa.pages;
+
+public class SearchPage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("I am search class");
+
+	}
+
+}
