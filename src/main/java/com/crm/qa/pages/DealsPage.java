f4177370c0f8ddcd87bac35570c058b4aaf06102
Su <ssvagle@gmail.com>
2020-07-19 13:30:43 +0530

    added new lines of code to the jaba class

--- src/main/java/com/crm/qa/pages/DealsPage.java
+++ src/main/java/com/crm/qa/pages/DealsPage.java
@@ -1,8 +1,16 @@
 package com.crm.qa.pages;
 
+import org.testng.annotations.Test;
+
 import com.crm.qa.base.TestBase;
 
 public class DealsPage extends TestBase {
+	
+	@Test
+	public void run()
+	{
+		System.out.println("added  new lines of codes ");
+	}
 
 
 }
